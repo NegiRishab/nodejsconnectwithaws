4f132bb80e3453ca4ad549e935ef6a345f08db34
Rishabh <rishabhvicky98@gmail.com>
2022-08-22 10:25:12 +0530

    hello monday again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send("welcome to my world");
+    res.send("welcome to my world monday");
 })
 
 const port=process.env.port ||3000;
